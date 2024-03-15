fde78fc7eea6a8309c2c6665a3408a98448393c2
typhix-dev <typhixdev@gmail.com>
2024-03-15 13:40:06 +0400

    ig business discovery

---
+++ ig-business-discovery/defines.php
@@ -0,0 +1,16 @@
+<?php
+	session_start();
+
+	define( 'FACEBOOK_APP_ID', '6251766671549288' );
+	define( 'FACEBOOK_APP_SECRET', '81f3c49efc10dc95329ee0f3432c296c' );
+	define( 'FACEBOOK_REDIRECT_URI', 'https://app1.ku.ac.ae/test-2' );
+	define( 'ENDPOINT_BASE', 'https://graph.facebook.com/v16.0/' );
+
+	// accessToken
+	$accessToken = 'EABY18tutI2gBAKL6uTFckNGgzkD0W6qf6bhHlbCVXjNW5PrCtCEj13o8kHguwlLVztrQiJkYBTZCdHZAVbXPVWjZCHHfOUcZCNxCVSU7cnzwNXPwmo1L9YmMFssislQjPCVk9Li7X7QbP644HBSEQHIoR7SqMjzvpvE3fjkA8vvTPeN6vOGBkyFAbGj5ZAmReTUitY2cKqYucq5NMWPm8mHkCQ4PuZAhqiAjvklnZC3zDU6h07hnwxt';
+
+	// page id
+	$pageId = '107402365349017';
+
+	// instagram business account id
+	$instagramAccountId = '17841446331469020';
